9abea8502a47b49f300905a0f7de51e90478a54b
Tony <ibtonybor@icloud.com>
2021-06-24 10:14:51 +0200

    - refactor all usages from the Word LED to Led - file names written with lowercase

--- lib/led.dart
+++ lib/led.dart
@@ -1,14 +1,14 @@
 import 'dart:convert';
 
-class LED {
+class Led {
   final List<int> rgbValues;
-  LED({required this.rgbValues});
+  Led({required this.rgbValues});
 
-  factory LED.empty() {
-    return LED(rgbValues: [0, 0, 0]);
+  factory Led.empty() {
+    return Led(rgbValues: [0, 0, 0]);
   }
-  factory LED.formJSON(List<dynamic> json) {
-    return LED(rgbValues: json.map((e) => e as int).toList());
+  factory Led.formJSON(List<dynamic> json) {
+    return Led(rgbValues: json.map((e) => e as int).toList());
   }
   setRGB(int r, int g, int b) {
     this.rgbValues[0] = r;
@@ -27,34 +27,34 @@ class LED {
 }
 
 class LEDs {
-  final List<LED> LEDValues;
-  LEDs({required this.LEDValues});
+  final List<Led> ledValues;
+  LEDs({required this.ledValues});
 
   factory LEDs.fromJSON(List<dynamic> json) {
-    return LEDs(LEDValues: json.map((e) => LED.formJSON(e)).toList());
+    return LEDs(ledValues: json.map((e) => Led.formJSON(e)).toList());
   }
 
   factory LEDs.unknown() {
-    return LEDs(LEDValues: List.generate(13, (index) => LED.empty()));
+    return LEDs(ledValues: List.generate(13, (index) => Led.empty()));
   }
 
   void allOFF() {
-    this.LEDValues.forEach((led) {
+    this.ledValues.forEach((led) {
       led.setRGB(0, 0, 0);
     });
   }
 
   void allON() {
-    this.LEDValues.forEach((led) {
+    this.ledValues.forEach((led) {
       led.setRGB(255, 255, 255);
     });
   }
 
   String toJson() {
     String json = "[";
-    for (int i = 0; i < this.LEDValues.length; i++) {
-      json = json + this.LEDValues[i].toJson();
-      if (i < this.LEDValues.length - 1) json = json + ",";
+    for (int i = 0; i < this.ledValues.length; i++) {
+      json = json + this.ledValues[i].toJson();
+      if (i < this.ledValues.length - 1) json = json + ",";
     }
     json = json + "]";
     print("json: ${json}");
